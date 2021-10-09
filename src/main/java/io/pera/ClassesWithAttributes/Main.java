b53069044c39bf5fd1d69ac04d28b058b54fbe08
Elif <elifcevik48@gmail.com>
2021-10-09 23:05:37 +0300

    Course 2 - Classes with attributes

---
+++ src/main/java/io/pera/ClassesWithAttributes/Main.java
@@ -0,0 +1,17 @@
+package io.pera.ClassesWithAttributes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Product product = new Product();
+        product.setName("Laptop");
+        product.setId(1);
+        product.setDescription("Asus Leptop");
+        product.setPrice(5000);
+        product.setStockAmount(3);
+
+        ProductManager productManager = new ProductManager();
+        productManager.add(product);
+    }
+
+}
---
+++ src/main/java/io/pera/ClassesWithAttributes/ProductManager.java
@@ -0,0 +1,9 @@
+package io.pera.ClassesWithAttributes;
+
+public class ProductManager {
+
+    public void add(Product product) {
+        System.out.println("Product added: " + product.getName());
+    }
+
+}
