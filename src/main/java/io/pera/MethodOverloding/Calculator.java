ad138f58c914e37f8790dd1b7446fbf4d4f9e906
Elif <elifcevik48@gmail.com>
2021-10-09 23:33:46 +0300

    Fix

--- src/main/java/io/pera/MethodOverloding/Calculator.java
+++ src/main/java/io/pera/MethodOverloding/Calculator.java
@@ -1,7 +1,7 @@
 package io.pera.MethodOverloding;
 
 public class Calculator {
-    
+
     public int sum(int number1, int number2) {
         return number1 + number2;
     }
@@ -9,5 +9,5 @@ public class Calculator {
     public int sum(int number1, int number2, int number3) {
         return number1 + number2 + number3;
     }
-    
+
 }
